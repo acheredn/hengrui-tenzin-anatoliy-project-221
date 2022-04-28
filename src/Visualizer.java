7c7d469f8f2c891c07639c0ac3c2f1886a8d329c
gyaltsentenzin <73610384+gyaltsen-tenzin@users.noreply.github.com>
2022-04-28 14:47:25 -0500

    Maze Visualizer completed : Need to implement animation now

--- src/Visualizer.java
+++ src/Visualizer.java
@@ -9,10 +9,22 @@ public class Visualizer {
     }
 
     public static void main(String[] args) {
-        CanvasWindow canvas = new CanvasWindow("test", 1000, 1000);
-        Rectangle pathExist = new Rectangle(10, 10, 100, 100);
-        pathExist.setFillColor(Color.BLACK);
-        canvas.add(pathExist);
+        CanvasWindow canvas = new CanvasWindow("Maze", 1000, 1000);
+
+        MazeGenerator maze = new MazeGenerator(10);
+        for (int i = 0; i < maze.getMaze().length; i++){
+            for (int j = 0; j < maze.getMaze()[0].length; j++) {
+                if (maze.getMaze()[i][j] == 1){
+                    Rectangle pathExist = new Rectangle(i*20, j*20, 20, 20);
+                    pathExist.setFillColor(Color.GREEN);
+                    canvas.add(pathExist);
+                } else {
+                    Rectangle pathNotExist = new Rectangle(i*20, j*20, 20, 20);
+                    pathNotExist.setFillColor(Color.RED);
+                    canvas.add(pathNotExist);
+                }
+            }
+        }
     }
     
     
