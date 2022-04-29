37d084ebab6f598e1e6a4777ced2632e553cefc5
gyaltsentenzin <73610384+gyaltsen-tenzin@users.noreply.github.com>
2022-04-29 09:56:48 -0500

    updated maze size

--- src/Visualizer.java
+++ src/Visualizer.java
@@ -9,17 +9,17 @@ public class Visualizer {
     }
 
     public static void main(String[] args) {
-        CanvasWindow canvas = new CanvasWindow("Maze", 1000, 1000);
+        CanvasWindow canvas = new CanvasWindow("Maze", 500, 500);
 
         MazeGenerator maze = new MazeGenerator(10);
         for (int i = 0; i < maze.getMaze().length; i++){
             for (int j = 0; j < maze.getMaze()[0].length; j++) {
                 if (maze.getMaze()[i][j] == 1){
-                    Rectangle pathExist = new Rectangle(i*20, j*20, 20, 20);
+                    Rectangle pathExist = new Rectangle(i*40, j*40, 40, 40);
                     pathExist.setFillColor(Color.GREEN);
                     canvas.add(pathExist);
                 } else {
-                    Rectangle pathNotExist = new Rectangle(i*20, j*20, 20, 20);
+                    Rectangle pathNotExist = new Rectangle(i*40, j*40, 40, 40);
                     pathNotExist.setFillColor(Color.RED);
                     canvas.add(pathNotExist);
                 }
